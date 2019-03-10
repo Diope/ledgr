dde2f7eebeb7ee3d4f4850e56c63298e324cc471
Dion Pettaway <dionp@fullsail.edu>
2019-03-09 21:31:50 -0500

    Set up routing for main pages of app created components folder need to figure out what will be containers

---
+++ src/components/EditBudget.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class EditBudget extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h1>
+        Edit An Existing Budget
+      </h1>
+     );
+  }
+}
+ 
+export default EditBudget;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+
 
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
+import CreateBudget from './components/CreateBudget'
+import AboutPage from './components/About'
+import EditBudget from './components/EditBudget'
+
 const routes = (
   <Router>
-    <Route path="/" component={App}/>
+    <div>
+      <Route path="/" component={App} exact={true}/>
+      <Route path="/create" component={CreateBudget} />
+      <Route path="/edit" component={EditBudget} />
+      <Route path="/about" component={AboutPage} />
+    </div>
   </Router>
 )
 
