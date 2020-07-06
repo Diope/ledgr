c7540bb7ff4c3ede0c5c784fb7d8a96b95c61818
Dion Pettaway <dionp@fullsail.edu>
2020-07-06 16:34:58 -0400

    preparing for netlify hosting

--- src/firebase/connect.js
+++ src/firebase/connect.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 let config = {
-  apiKey: "AIzaSyALDpA2Agm-4b9G1B6nyRPIEDpvHvZ04oM",
-    authDomain: "financial-budgeting.firebaseapp.com",
-    databaseURL: "https://financial-budgeting.firebaseio.com",
-    projectId: "financial-budgeting",
-    storageBucket: "financial-budgeting.appspot.com",
-    messagingSenderId: "950138150712"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
